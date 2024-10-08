f2fd69a31dfa64b65697b38a81e12417fc9e3e0a
Надя <nadya.ptz@gmail.com>
2024-10-08 12:12:45 +0300

    доработанный вариант

--- Module_10_4.py
+++ Module_10_4.py
@@ -34,18 +34,20 @@ class Cafe:
         if not table_empty:
             return None
 
-    def all_tables_are_empty(self):  # проверяем, все ли столы пустые
-        for t in self.tables:  # если хоть один занят - возвращаем False
-            if t.guest is not None:
-                return False
-        return True  # если все перебрали и гостей нет, то возвращаем True
+    # def all_tables_are_empty(self):  # проверяем, все ли столы пустые
+    #     for t in self.tables:  # если хоть один занят - возвращаем False
+    #         if t.guest is not None:
+    #             return False
+    #     return True  # если все перебрали и гостей нет, то возвращаем True
+    def __all_tables_are_empty(self):  # проверяем, все ли столы пустые
+        return all(map(lambda table: table.guest is None, self.tables))
 
     def guest_arrival(self, *guests):
         for g in guests:
-            if self.check_if_any_table_is_empty():  # если находим пустой стол
-                t = self.check_if_any_table_is_empty()  # возвращаем его номер
-                t.guest = g.guest_name  # за стол сажаем гостя
-                print(f'{t.guest} сел(-а) за стол номер {t.table_number}')
+            t = self.check_if_any_table_is_empty()
+            if t is not None:  # если находим пустой стол
+                t.guest = g  # за стол сажаем гостя
+                print(f'{g.guest_name} сел(-а) за стол номер {t.table_number}')
                 g.start()  # стартуем поток этого гостя
             else:
                 self.queue.put(g)  # если нет пустого стола - ставим гостя в очередь
@@ -53,17 +55,16 @@ class Cafe:
 
     def serve_guests(self):
         while not self.queue.empty() or not self.all_tables_are_empty():  # пока очередь не пуста и хотя бы один стол не пустой
-            for t in tables:        # перебираем столы
-                for g in guests:    # перебираем гостей
-                    if t.guest is not None and t.guest == g.guest_name and not g.is_alive(): # если за столом есть гость и он закончил свой поток
-                        print(f'{t.guest} покушал(-а) и ушёл(ушла)')    # то освобождаем стол
-                        print(f'Стол номер {t.table_number} свободен')
-                        t.guest = None
-                    if not self.queue.empty() and t.guest is None:      # если находим пустой стол и очередь не пуста
-                        guest = self.queue.get()                        # берем гостя из очереди
-                        t.guest = guest.guest_name
-                        print(f'{guest.guest_name} вышел(-ла) из очереди и сел(-а) за стол номер {t.table_number}')
-                        guest.start()                                      # сажаем за стол и начинаем поток этого гостя
+            for t in tables:  # перебираем столы
+                if t.guest is not None and not t.guest.is_alive():  # если за столом есть гость и он закончил свой поток
+                    print(f'{t.guest.guest_name} покушал(-а) и ушёл(ушла)')  # то освобождаем стол
+                    print(f'Стол номер {t.table_number} свободен')
+                    t.guest = None
+                if not self.queue.empty() and t.guest is None:  # если находим пустой стол и очередь не пуста
+                    guest = self.queue.get()  # берем гостя из очереди
+                    t.guest = guest
+                    print(f'{guest.guest_name} вышел(-ла) из очереди и сел(-а) за стол номер {t.table_number}')
+                    guest.start()  # сажаем за стол и начинаем поток этого гостя
 
 
 # Создание столов
